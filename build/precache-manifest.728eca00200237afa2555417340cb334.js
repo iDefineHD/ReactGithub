94097f01424939987fa14d983c2e85c7c60e8c77
Ashley Bridges <purpleizlegitx@gmail.com>
2020-09-12 13:13:29 +0100

    Added Build Folder.

---
+++ build/precache-manifest.728eca00200237afa2555417340cb334.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01719a80d1502cb51113a9e0e422f227",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bcc91c03b1741ff70bee",
+    "url": "/static/css/main.167fc0d5.chunk.css"
+  },
+  {
+    "revision": "8b15da9b606ba11cd3b0",
+    "url": "/static/js/2.0f03f8c2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.0f03f8c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcc91c03b1741ff70bee",
+    "url": "/static/js/main.7a764309.chunk.js"
+  },
+  {
+    "revision": "a3d2537862ba82e489cd",
+    "url": "/static/js/runtime-main.0e897540.js"
+  }
+]);
